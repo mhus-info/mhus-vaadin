86f9fcf9e872e4aba0e83644d05d1a9387043563
Mike Hummel <>
2019-07-26 23:24:06 +0200

    working example

--- mhu-vaadin-osgi/src/main/java/de/mhus/vaadin/osgi/MainView.java
+++ mhu-vaadin-osgi/src/main/java/de/mhus/vaadin/osgi/MainView.java
@@ -5,14 +5,22 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 @Route("")
 @PWA(name = "Project Base for Vaadin Flow", shortName = "Project Base")
+@Theme(Lumo.class)
 public class MainView extends VerticalLayout {
+
+    private static final long serialVersionUID = 1L;
 
     public MainView() {
         Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
+                event -> {
+                    System.out.println("Clicked");
+                    Notification.show("Clicked!");
+                });
         add(button);
     }
 }
--- mhu-vaadin-osgi/src/main/java/de/mhus/vaadin/osgi/VaadinServlet.java
+++ mhu-vaadin-osgi/src/main/java/de/mhus/vaadin/osgi/VaadinServlet.java
@@ -1,17 +1,20 @@
 package de.mhus.vaadin.osgi;
 
 import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 
 import org.osgi.service.component.annotations.Component;
-
-import com.vaadin.flow.server.VaadinServletConfiguration;
-
 
 @Component(service=Servlet.class,property= "alias=/hello",servicefactory=true)
 public class VaadinServlet extends com.vaadin.flow.server.VaadinServlet {
 
     private static final long serialVersionUID = 1L;
+    
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        super.init(servletConfig);
 
-    
-    
+        getService().setClassLoader(getClass().getClassLoader());
+    }
 }
